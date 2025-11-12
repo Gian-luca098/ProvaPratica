da04b05a41699f4a9123cec9e6eaa20ae52ea423
Gianluca <rossogian62@gmail.com>
2025-11-11 21:32:01 -0300

    Add files via upload

---
+++ Prova-pratica-Finalizada-main/Prova-Pratica-main/Mjogos.js
@@ -0,0 +1,64 @@
+alert("Bem vindo à seção de jogos Mais jogados!");
+const meuBotao = document.getElementById('meuBotao');
+const textoSecreto = document.getElementById('textoSecreto');
+
+if (meuBotao && textoSecreto) {
+    meuBotao.addEventListener('click', function () {
+        textoSecreto.classList.add('visivel');
+    });
+} else {
+    console.warn('Elemento(s) não encontrado(s):', { meuBotao, textoSecreto });
+}
+//bottao 2
+const meuBotao1 = document.getElementById('meuBotao1');
+const textoSecreto1 = document.getElementById('textoSecreto1');
+
+if (meuBotao1 && textoSecreto1) {
+    meuBotao1.addEventListener('click', function () {
+        textoSecreto1.classList.add('visivel');
+    });
+} else {
+  
+    console.warn('Elemento(s) não encontrado(s):', { meuBotao1, textoSecreto1 });
+}
+//bottao 3
+const meuBotao2 = document.getElementById('meuBotao2');
+const textoSecreto2 = document.getElementById('textoSecreto2');
+
+if (meuBotao2 && textoSecreto2) {
+    meuBotao2.addEventListener('click', function () {
+        textoSecreto2.classList.add('visivel');
+    });
+} else {
+   
+    console.warn('Elemento(s) não encontrado(s):', { meuBotao2, textoSecreto2 });
+}
+const imagem = document.getElementById('card');
+
+imagem.addEventListener('mouseenter', () => {
+  imagem.style.transform = 'scale(1.5)';
+});
+
+imagem.addEventListener('mouseleave', () => {
+  imagem.style.transform = 'scale(1)';
+});
+
+const imagem1 = document.getElementById('card1');
+
+imagem1.addEventListener('mouseenter', () => {
+  imagem1.style.transform = 'scale(1.5)';
+});
+
+imagem1.addEventListener('mouseleave', () => {
+  imagem1.style.transform = 'scale(1)';
+});
+
+const imagem2 = document.getElementById('card2');
+
+imagem2.addEventListener('mouseenter', () => {
+  imagem2.style.transform = 'scale(1.5)';
+});
+
+imagem2.addEventListener('mouseleave', () => {
+  imagem2.style.transform = 'scale(1)';
+});
---
+++ Prova-pratica-Finalizada-main/Prova-Pratica-main/inicio.js
@@ -0,0 +1,30 @@
+alert("Bem vindo ao Inicio do Mygames!");
+const imagem = document.getElementById('card');
+
+imagem.addEventListener('mouseenter', () => {
+  imagem.style.transform = 'scale(1.5)';
+});
+
+imagem.addEventListener('mouseleave', () => {
+  imagem.style.transform = 'scale(1)';
+});
+
+const imagem1 = document.getElementById('card1');
+
+imagem1.addEventListener('mouseenter', () => {
+  imagem1.style.transform = 'scale(1.5)';
+});
+
+imagem1.addEventListener('mouseleave', () => {
+  imagem1.style.transform = 'scale(1)';
+});
+
+const imagem2 = document.getElementById('card2');
+
+imagem2.addEventListener('mouseenter', () => {
+  imagem2.style.transform = 'scale(1.5)';
+});
+
+imagem2.addEventListener('mouseleave', () => {
+  imagem2.style.transform = 'scale(1)';
+});
